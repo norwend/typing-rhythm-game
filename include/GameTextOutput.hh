4e49856d21d8dc7fb59ba6fe5f6280ae437483e0
Stepan Ilyin <norwend@tuta.io>
2022-11-30 15:13:19 +0300

    Add a Drawable interface and start working on GameTextOutput

--- include/GameTextOutput.hh
+++ include/GameTextOutput.hh
@@ -1,13 +1,21 @@
 #pragma once
 
+#include "Drawable.hh"
+#include "SFML/Graphics/Color.hpp"
+
 /* TODO:
  * сделать текст светящимся при нажатии верной клавиши;
- * переименовать в GameTextOutput;
  * функция highlightLetter() должна быть! её надо будет менять!;
  * сделать GameTextInput;
  * для обычного GUI использовать обычный sf::Text.
  * */
 
-class GameTextOutput {
+class GameTextOutput : public Drawable {
+private:
+    int x;
+    int y;
+    int mCharSize;
+    sf::Color mColor;
+public:
 
 };
