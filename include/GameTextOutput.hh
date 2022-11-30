d4fcaa8ceb82bf74dfef7e8834394427547bd522
Stepan Ilyin <norwend@tuta.io>
2022-11-30 14:54:43 +0300

    TextOutput renamed to GameTextOutput

---
+++ include/GameTextOutput.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+/* TODO:
+ * сделать текст светящимся при нажатии верной клавиши;
+ * переименовать в GameTextOutput;
+ * функция highlightLetter() должна быть! её надо будет менять!;
+ * сделать GameTextInput;
+ * для обычного GUI использовать обычный sf::Text.
+ * */
+
+class GameTextOutput {
+
+};
--- src/TextOutput.cc
+++
@@ -1,1 +0,0 @@
-#include <TextOutput.hh>
